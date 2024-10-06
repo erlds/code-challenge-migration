31aa9ea029656c68c33e6b0bbf4656113f3c2c78
evaristo <erlds5518@gmail.com>
2024-10-06 17:23:22 -0300

    Add id attribute

--- src/test/java/com/example/dummyjson/controller/ProductControllerTest.java
+++ src/test/java/com/example/dummyjson/controller/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.dummyjson.controller;
 
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,17 +20,24 @@ public class ProductControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    private int id;
+
+    @BeforeEach
+    public void setUp() {
+        id = 1;
+    }
+
     @Test
     public void testGetAllProducts() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get(URI))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id", Matchers.is(1)));
+                .andExpect(jsonPath("$[0].id", Matchers.is(id)));
     }
 
     @Test
     public void testGetProductById() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get(URI + "/1"))
+        mvc.perform(MockMvcRequestBuilders.get(URI + "/" + id))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("id", Matchers.is(1)));
+                .andExpect(jsonPath("id", Matchers.is(id)));
     }
 }
